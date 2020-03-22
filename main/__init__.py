d86c3ec3279bc408c8aed563bec211786955b62e
kazuyamano <kazuyamano@MacBookPro.local>
2020-03-22 19:08:56 +0900

    flask解説分を追加

--- main/__init__.py
+++ main/__init__.py
@@ -1,8 +1,18 @@
-from flask import Flask
+# まず、 Flask クラスをインポートしました。このクラスのインスタンスは WSGIアプリケーションです。
+#[このアプリケーションのモジュール名について]
+# もしあなたが使うモジュールが一つだけなら、 __name__ を使わなければなりません。
+# それがアプリケーションとして起動したときとモジュールとしてインポートされたときで名前が異なるからです
+#  (アプリケーションとして起動したときは '__main__' 、インポートされたときはそのインポート名)
+# from flask import Flask
+
+from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
 
+# 次にインスタンスを生成します。モジュールやパッケージの名前は要りません。このインスタンスは
+# Flaskがテンプレートファイルやスタティックファイルなどをどこから探すのかを認識するために必要です。
+# app = Flask(__name__)
 app = Flask(__name__)
 app.config.from_object('main.config')
-db=SQLAlchemy(app)
+db = SQLAlchemy(app)
 
 import main.views
--- main/views.py
+++ main/views.py
@@ -3,10 +3,18 @@ from main import app, db
 from main import app
 from main.models import Entry
 
+# route() デコレータを使用し、ファンクションを起動するURLをFlaskに教えます。
+# そのファンクション名は特定のファンクションに対してURLを生成するためにも使われ、 
+# ファンクションはユーザーのブラウザ上で表示したいメッセージを返します。
+# @app.route('/')
+# def hello_world():
+#     return "Hello World!"
+
 @app.route('/')
 def show_entries():
     entries = Entry.query.all()
     return flask.render_template('entries.html', entries=entries)
+
 
 # add_entry 関数の追加
 @app.route('/add', methods=['POST'])
