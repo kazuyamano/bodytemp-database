541fc2b81601c2640b7909ccdc8860597f997065
やまの <ymkzjn@gmail.com>
2020-04-29 10:33:33 +0900

    あああ

--- main/__init__.py
+++ main/__init__.py
@@ -3,14 +3,13 @@
 # もしあなたが使うモジュールが一つだけなら、 __name__ を使わなければなりません。
 # それがアプリケーションとして起動したときとモジュールとしてインポートされたときで名前が異なるからです
 #  (アプリケーションとして起動したときは '__main__' 、インポートされたときはそのインポート名)
-# from flask import Flask
 
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
 
 # 次にインスタンスを生成します。モジュールやパッケージの名前は要りません。このインスタンスは
 # Flaskがテンプレートファイルやスタティックファイルなどをどこから探すのかを認識するために必要です。
-# app = Flask(__name__)
+
 app = Flask(__name__)
 app.config.from_object('main.config')
 db = SQLAlchemy(app)
