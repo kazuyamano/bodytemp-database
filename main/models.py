c5436c87ed370a493bbd4a1a7d4c35114ee19fb5
やまの <ymkzjn@gmail.com>
2020-05-17 16:28:12 +0900

    DBのカラム名変更対応　 models.pyのtitle=>jcode

--- main/models.py
+++ main/models.py
@@ -8,7 +8,7 @@ class Entry(db.Model):
     text = db.Column(db.Text)
 
     def __repr__(self):
-        return "<Entry id={} title={!r}>".format(self.id, self.title)
+        return "<Entry id={} jcode={!r}>".format(self.id, self.jcode)
 
 
 def init():
