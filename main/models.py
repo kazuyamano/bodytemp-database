75d3ac30ea45b3ef692977454512dea70f83bb11
やまの <ymkzjn@gmail.com>
2020-05-17 17:03:05 +0900

    戻し

--- main/models.py
+++ main/models.py
@@ -8,7 +8,7 @@ class Entry(db.Model):
     text = db.Column(db.Text)
 
     def __repr__(self):
-        return "<Entry id={} jcode={!r}>".format(self.id, self.jcode)
+        return "<Entry id={} title={!r}>".format(self.id, self.title)
 
 
 def init():
