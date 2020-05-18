0a4503f1cd94dc1429e1107235be86180c151fab
やまの <ymkzjn@gmail.com>
2020-05-19 00:51:08 +0900

    文言修正

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
-#全体に参照したサイト「Flaskチュートリアル + herokuにデプロイ」
+# 全体に参照したサイト「Flaskチュートリアル + herokuにデプロイ」
 # https://qiita.com/yuucu/items/dea6e7a0a655af8fdb91
 
 
-#run.pyは、アプリケーションをデバッグモードで起動するだけのプログラムです。
+# run.pyは、アプリケーションをデバッグモードで起動するだけのプログラムです。
 
 from main import app
 
